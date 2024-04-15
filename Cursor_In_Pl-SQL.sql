d8061580ccf7aa60551693dc511dcdd2e70bdd43
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-04-15 11:03:04 +0530

    Implicit cursor in pl-sql

--- Cursor_In_Pl-SQL.sql
+++ Cursor_In_Pl-SQL.sql
@@ -1,1 +1,14 @@
+-- Cursor in PL/SQL
+-- Two type of curosr in PL/SQL
+-- 1.Implicit cursor
+-- 2.explicit curosr
 
+-- 1. Implicit cursor
+declare
+e_name employee.emp_name%type;
+begin
+  select emp_name into e_name 
+  from employee
+  where emp_id = :emp_id_no;
+  dbms_output.put_line('Employee details is : -' || e_name);
+end;
