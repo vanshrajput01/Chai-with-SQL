345e1eebbd8ef03d66063a92ef5805f6f71de57b
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-04-15 11:07:40 +0530

    Explicit cursor in PL-SQL

--- Cursor_In_Pl-SQL.sql
+++ Cursor_In_Pl-SQL.sql
@@ -12,3 +12,33 @@ begin
   where emp_id = :emp_id_no;
   dbms_output.put_line('Employee details is : -' || e_name);
 end;
+
+
+-- Explicit Cursor 
+-- these are two ways to define explicit Cursor
+-- 1. OPEN , FETCH and CLOSE 
+-- 2.loop
+
+
+
+-- if fetch single trecord in table;
+declare
+e_id employee.emp_id%type;
+cursor c1 is
+  select emp_id from employee;
+begin
+  open c1;
+    fetch c1 into e_id;
+    dbms_output.put_line(c1%rowcount || '-' || e_id);
+  close c1;
+end;
+
+
+
+
+
+
+
+
+
+
