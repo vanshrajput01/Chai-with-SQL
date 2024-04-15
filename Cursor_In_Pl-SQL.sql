c2affc9fbe7c23c4454e75398e2283221395a67f
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-04-15 11:28:21 +0530

    fetch all record from table using fetch method.

--- Cursor_In_Pl-SQL.sql
+++ Cursor_In_Pl-SQL.sql
@@ -20,8 +20,10 @@ end;
 -- 2.loop
 
 
+-- Example 1. +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
--- if fetch single trecord in table;
+
+-- if fetch single trecord in table OR know how many record in the table;
 declare
 e_id employee.emp_id%type;
 cursor c1 is
@@ -30,8 +32,51 @@ begin
   open c1;
     fetch c1 into e_id;
     dbms_output.put_line(c1%rowcount || '-' || e_id);
+    fetch c1 into e_id;
+    dbms_output.put_line(c1%rowcount || '-' || e_id);
+    fetch c1 into e_id;
+    dbms_output.put_line(c1%rowcount || '-' || e_id);
   close c1;
 end;
+
+
+-- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Example 2 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+-- fetch the multiple record in table using loop.
+declare
+e_id employee.emp_id%type;
+e_name employee.emp_name%type;
+
+cursor c1 is
+select emp_id,emp_name from employee;
+
+begin
+  open c1;
+  loop
+  fetch c1 into e_id,e_name;
+  exit when c1%notfound;
+  dbms_output.put_line(c1%rowcount || '-' || e_id || ' ' || e_name);
+  end loop;
+  close c1;
+end;
+
+-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Example 03 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+declare
+emp employee%rowtype;
+
+cursor c1 is
+select * from employee;
+
+begin
+open c1;
+loop
+fetch c1 into emp;
+exit when c1%notfound;
+dbms_output.put_line('Employee details : -' ||  emp.emp_id|| ' ' || emp.emp_name || ' ' || emp.emp_salary || ' ' || emp.emp_position);
+end loop;
+close c1;
+end;
+ 
 
 
 
