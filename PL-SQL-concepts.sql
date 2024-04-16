550be1be9d1d0ca6e7a61720c7dab25ad39b8661
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-04-16 10:33:35 +0530

    Create procedure in PL/SQL "Second commit"

--- PL-SQL-concepts.sql
+++ PL-SQL-concepts.sql
@@ -23,6 +23,31 @@ dbms_output.put_line('total num' || '-' || total);
 
 end;
 
+--++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++            Procedure in PL/SQL            ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+--Example 01 :- thing Table is already created and using procedure to insert data;
+
+
+-- Here create or replace Procedure 
+create or replace procedure insert_data_in_table as
+e_id int := 101;
+e_name varchar(20) := 'Jockey';
+e_salary int := 10000;
+e_position varchar(30) := 'developer-intern';
+
+begin
+insert into employee values(e_id,e_name,e_salary,e_position);
+dbms_output.put_line('Data insert into employee successfully!!');
+end;
+
+ 
+-- Here Completed Procedure
+begin
+insert_data_in_table;
+end;
+
+
+
+
 
 
 
