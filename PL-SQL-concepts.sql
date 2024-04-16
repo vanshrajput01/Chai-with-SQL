2b5728ba0c94063aef1e1dd8c5473f389a248935
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-04-16 10:23:24 +0530

    Anonymous code block

--- PL-SQL-concepts.sql
+++ PL-SQL-concepts.sql
@@ -1,1 +1,13 @@
+-- Anonymous code block in PL/SQL
+declare
+num1 int := :num1;
+num2 int := :num2;
+total int;
+
+begin
+total := num1 + num2;
+dbms_output.put_line('total num' || '-' || total);
+
+end;
+
 
