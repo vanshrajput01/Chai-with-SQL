0dbe591c9972ca065f820678cfc3c33a31cea2a0
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-04-19 11:43:07 +0530

    Second commit

--- Mutating-Error-In-PL-SQL.sql
+++ Mutating-Error-In-PL-SQL.sql
@@ -44,6 +44,59 @@ update employee
 set emp_salary = 3000
 where emp_id = 103;
 
+-- Mutating Error in Triggers 
+
+--Example 02 : - 
+-- ******************************                     Mutating Error in Triggers ********************************************************
+ 
+
+create or replace trigger bef_update_emp_trigger
+before
+update
+on employee
+for each row
+
+declare
+
+max_salary number;
+
+begin
+
+select emp_salary into max_salary 
+from employee
+where emp_salary = 500000;
+
+if :new.emp_salary >= max_salary then
+dbms_output.put_line('You can not assign the salary.');
+RAISE_APPLICATION_ERROR(-20100, 'You can not assign this salary to this employee');
+else
+dbms_output.put_line('You are assign the salary success fully.');
+end if;
+end;
+
+
+-- Here i am update min salary of this employee but this code give an Error 
+update employee
+set emp_salary = 3000
+where emp_id = 103;
+
+
+-- Error is : -
+
+Error starting at line : 31 in command -
+update employee
+set emp_salary = 3000
+where emp_id = 103
+Error report -
+ORA-04091: table APPS.EMPLOYEE is mutating, trigger/function may not see it
+ORA-06512: at "APPS.BEF_UPDATE_EMP_TRIGGER", line 7
+ORA-04088: error during execution of trigger 'APPS.BEF_UPDATE_EMP_TRIGGER'
+
+
+ -- ***************************************************              This is Example of # Mutating Error      ******************************************************
+
+
+
 
 
 
