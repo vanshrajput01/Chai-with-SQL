38ab0924031ddaef6b51db45f4644cbe8738acae
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-04-19 11:38:54 +0530

    "First commit"

---
+++ Mutating-Error-In-PL-SQL.sql
@@ -0,0 +1,51 @@
+-- Mutating Error in Triggers 
+
+--Example 01 : - 
+-- this is not example of mutating Error in Cursor.
+
+
+create or replace trigger bef_update_emp_trigger
+before
+update
+on employee
+for each row
+
+declare
+
+max_salary number := 30000;
+
+begin
+if :new.emp_salary >= max_salary then
+dbms_output.put_line('You can not assign the salary.');
+RAISE_APPLICATION_ERROR(-20100, 'You can not assign this salary to this employee');
+else
+dbms_output.put_line('You are assign the salary success fully.');
+end if;
+end;
+
+update employee
+set emp_salary = 550000
+where emp_id = 103;
+
+-- **************************************         That Raise Error like **************************************************************************
+
+Error starting at line : 26 in command -
+update employee
+set emp_salary = 550000
+where emp_id = 103
+Error report -
+ORA-20100: You can not assign this salary to this employee
+ORA-06512: at "APPS.BEF_UPDATE_EMP_TRIGGER", line 8
+ORA-04088: error during execution of trigger 'APPS.BEF_UPDATE_EMP_TRIGGER'
+
+
+-- Here Not give Error ok
+update employee
+set emp_salary = 3000
+where emp_id = 103;
+
+
+
+
+
+
