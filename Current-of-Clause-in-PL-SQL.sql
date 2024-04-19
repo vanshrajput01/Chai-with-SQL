4551808bc22d871d874df2d02f4444612de54e52
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-04-19 12:10:01 +0530

    Thrid commit

--- Current-of-Clause-in-PL-SQL.sql
+++ Current-of-Clause-in-PL-SQL.sql
@@ -44,6 +44,33 @@ end;
 -- *********************************************  that why i am using ***************        Current of Clause        ******************************************
 
 
+-- Example of current of clause
+
+--Current of Clause 
+
+declare
+
+cursor c1 is 
+select * from employee
+where dept_id = 3
+for update;
+
+begin
+for i in c1 loop
+dbms_output.put_line('Employee details is where department id is 3 ' || i.emp_name || ' ' || i.emp_salary || ' ' ||i.dept_id);
+
+update employee
+set emp_salary = 1000
+where current of c1;
+end loop;
+end;
+
+
+
+
+
+
+
 
 
 
