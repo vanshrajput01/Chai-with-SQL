3257ad5eaf1b936046af588ac01630ad2350d0e8
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-04-19 12:09:22 +0530

    second commit

--- Current-of-Clause-in-PL-SQL.sql
+++ Current-of-Clause-in-PL-SQL.sql
@@ -17,6 +17,32 @@ end loop;
 end;
 
 
+--  *********************************************              if not know employee id that update all rows           *****************************************************
+
+
+--Current of Clause 
+
+declare
+
+cursor c1 is 
+select * from employee
+where dept_id = 3;
+
+begin
+for i in c1 loop
+dbms_output.put_line('Employee details is where department id is 3 ' || i.emp_name || ' ' || i.emp_salary || ' ' ||i.dept_id);
+
+update employee
+set emp_salary = 100;
+
+end loop;
+end;
+
+
+--  *********************************************              if not know employee id that update all rows but my task not update all row.          *****************************************************
+
+-- *********************************************  that why i am using ***************        Current of Clause        ******************************************
+
 
 
 
