9fa05499b08eb2a86786705aa40791afab52c067
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-04-19 11:22:50 +0530

    For update clause in PL-SQL " Second commit"

--- for-Update-clause-in-PL-SQL.sql
+++ for-Update-clause-in-PL-SQL.sql
@@ -22,3 +22,33 @@ end;
 delete from employee
 where dept_id = 2;
 
+
+
+
+--  *************************************************************           For Update clause in PL/SQL  *************************************************************************
+
+-- For update Clause.
+declare
+cursor c1 is
+select * from employee
+where dept_id = 2
+for update; --ye task lock kar deta ha.
+
+begin
+for i in c1 loop
+dbms_lock.sleep(10);  --Program wait 10 sec and then perform these below tasks.
+dbms_output.put_line('employee details is : - '|| i.emp_name || ' ' || i.emp_salary);
+update employee
+set emp_salary = 2500
+where dept_id = 2;
+dbms_output.put_line('Update employee details is '||i.emp_name || ' ' || i.emp_salary);
+end loop;
+
+end;
+
+-- ye task abi parallel run hoga nahi hoga. first above task complete hoga than next task run hoga.
+delete from employee
+where dept_id = 2;
+
+select * from employee;
+
