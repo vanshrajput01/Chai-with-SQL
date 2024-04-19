921ed6979b33c316d87c6f3655b4757774d02207
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-04-19 16:01:15 +0530

    thrid commit

--- Execute-Immediate-In-PL-SQL.sql
+++ Execute-Immediate-In-PL-SQL.sql
@@ -49,7 +49,25 @@ begin
 DYNAMIC_EMP_PROCE(1);
 end;
 
---Dynamic SQL --- Here i am pass table as Parameter
+--***************************************************************          Dynamic SQL --- Here i am pass table as Parameter   ************************************************
+
+-- This code give an Error 
+create or replace procedure dynamic_emp_proce_2 as
+v_count number;
+begin
+select count(*) into v_count from employee
+where dept_id = :v_dept_id;  -- This line give me Error
+dbms_output.put_line(v_count);
+end;
+
+LINE/COL  ERROR
+--------- -------------------------------------------------------------
+5/17      PLS-00049: bad bind variable 'V_DEPT_ID'
+Errors: check compiler log
+
+  
+
+-- *******************************************************                 Dynamic SQL --- Here i am pass table as Parameter   *****************************************************
 
 -- This code give an Error 
 
@@ -68,8 +86,30 @@ LINE/COL  ERROR
 4/35      PL/SQL: ORA-00942: table or view does not exist
 Errors: check compiler log
 
+-- *****************************************************************          To solved that problem using Execute Immediate Concept        *********************************************
 
--- 
+----Execute Immediate in PL/SQL
+
+CREATE OR REPLACE PROCEDURE dynamic_emp_proce4 (
+    v_dept_id NUMBER,
+    v_table_name VARCHAR2
+) AS
+    v_count NUMBER;
+    v_sql VARCHAR2(300); -- Corrected variable type declaration
+BEGIN
+    v_sql := 'SELECT COUNT(*) FROM ' || v_table_name || ' WHERE dept_id = :deptno'; -- Added spaces
+    EXECUTE IMMEDIATE v_sql INTO v_count USING v_dept_id;
+    
+    dbms_output.put_line(v_count);
+END;
+
+
+
+begin
+DYNAMIC_EMP_PROCE4(1,'employee');
+end;
+
+
 
 
 
