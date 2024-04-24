9fd12b66f5a9cb08af9c3794cb5fee71e81d836b
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-04-24 07:04:20 +0530

    first commit

--- PL-SQL Concepts02.sql
+++ PL-SQL Concepts02.sql
@@ -1,1 +1,21 @@
--- PL/SQL Concepts 
+-- PL/SQL Concepts
+
+-- Anonymous Block in PL-SQL
+-- These are two type of Anonymous Block in PLS/SQL
+
+-- 01. Static Anonymous Block :- 
+-- Example 01 :- 
+declare
+v_num1 number:= 10;
+v_num2 number:= 20;
+v_total number;
+begin
+v_total := v_num1 + v_num2;
+dbms_output.put_line(v_total);
+
+exception 
+when others then
+dbms_output.put_line('Error' || SQLERRM);
+end;
+
+
