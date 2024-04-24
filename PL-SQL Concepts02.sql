b7d4d962b2d5e6a360b9e6e823d16ef40dd3a355
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-04-24 07:10:01 +0530

    second commit "Dynamic Anonymous Block"

--- PL-SQL Concepts02.sql
+++ PL-SQL Concepts02.sql
@@ -19,3 +19,21 @@ dbms_output.put_line('Error' || SQLERRM);
 end;
 
 
+-- 02. Dynamic Anonymous Block
+
+-- Example 01 :-
+
+declare
+v_num1 number := :value1; --Bind Variable
+v_num2 number := :value2;  --Bind Variable
+v_total number;
+begin
+v_total := v_num1+ v_num2;
+dbms_output.put_line(v_total);
+exception
+when others then
+dbms_output.put_line('Error' || SQLERRM);
+end;
+
+
+
