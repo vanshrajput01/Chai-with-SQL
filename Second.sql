861118d974a7dbdd4a49141bf301319ade9fde03
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-04-26 13:07:42 +0530

    Not null constraint in sql

--- Second.sql
+++ Second.sql
@@ -1,1 +1,35 @@
 -- Advance SQL
+
+=================================================================   Constraints in SQL  ==============================================================================================
+
+-- These are mainly 6-7 type of constraint in sql
+
+1.NOT NULL
+
+======================================================================   NOT NULL CONSTRAINTS IN SQL ================================================================================
+-- Example 01 := 
+
+  -- at creation time
+
+CREATE table employee(
+  emp_id number NOT NULL,
+  emp_name varchar(30),
+  emp_salary number
+);
+
+
+-- IF table is already created How to add assign Constraint to column:- 
+
+alter table employee_ex
+modify emp_salary number not null;
+
+
+-- if table is created 
+
+alter table employee_ex
+add dept_id number not null;
+
+===========================================================
+
+
+
