9a91ba38a87e1e871efd30c385798fe3e33bf255
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-04-30 08:00:47 +0530

    Thrid.sql  'second commit'

--- Thrid.sql
+++ Thrid.sql
@@ -1,1 +1,7 @@
 -- initial commit
+========================================================   Insert value in table (Another way)  ========================================================================================
+
+insert into employee_ex(emp_id,emp_name,emp_salary)(select emp_id,emp_name,emp_salary from employee);
+
+select * from  employee;
+select * from employee_ex;
