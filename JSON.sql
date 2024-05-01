c980147d2729469bf06d66b4adea9f8f859b61ae
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-05-01 13:27:05 +0530

    JSON in PL-SQL 'get_count'

--- JSON.sql
+++ JSON.sql
@@ -57,3 +57,23 @@ dbms_output.put_line(v_emp_name);
 end;
 
 
+=============================================================================  Get_count ============================================================================================
+
+--Apex_Json
+
+declare
+json_string clob := '{"fname" : "mark","lname" : "hook","salary" : 10000,"id" : 1}';
+json_obj apex_json.t_values;
+emp_first_name varchar(50);
+emp_last_name varchar(50);
+emp_id number;
+begin
+apex_json.parse(json_obj,json_string);
+emp_first_name := apex_json.get_varchar2(p_path => 'fname' , p_values => json_obj);
+emp_last_name := apex_json.get_varchar2(p_path => 'lname' , p_values => json_obj);
+dbms_output.put_line('Employee full name is  : '|| emp_first_name || ' ' || emp_last_name);
+dbms_output.put_line(apex_json.get_count(p_path => '.' , p_values => json_obj));
+
+
+end;
+
