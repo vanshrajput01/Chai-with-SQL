24fedcefc6b1e6f3c3a367a5139052137e06980e
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-05-01 13:00:53 +0530

    JSON in pl-sql "Example 02"

--- JSON.sql
+++ JSON.sql
@@ -18,3 +18,23 @@ v_age := apex_json.get_number(p_path => 'age' , p_values => json_obj);
 dbms_output.put_line('name is : -' || v_name);
 dbms_output.put_line('age is : -' || v_age);
 end;
+
+
+=====================================================================================  Example 02 ==============================================================================
+
+--Apex_Json
+
+declare
+json_string clob := '{"fname" : "mark","lname" : "hook","salary" : 10000,"id" : 1}';
+json_obj apex_json.t_values;
+emp_first_name varchar(50);
+emp_last_name varchar(50);
+emp_id number;
+begin
+apex_json.parse(json_obj,json_string);
+emp_first_name := apex_json.get_varchar2(p_path => 'fname' , p_values => json_obj);
+emp_last_name := apex_json.get_varchar2(p_path => 'lname' , p_values => json_obj);
+dbms_output.put_line('Employee full name is  : '|| emp_first_name || ' ' || emp_last_name);
+
+
+end;
