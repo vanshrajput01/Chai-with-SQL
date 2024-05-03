58ef19cc1cc9fde7b95e5182108c3de73969d4d3
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-05-03 10:14:06 +0530

    first_example

--- PL-SQL_Concept.sql
+++ PL-SQL_Concept.sql
@@ -1,1 +1,11 @@
 -- Execute immediate
+-- Example 01 :- 
+
+declare
+v_sql varchar(200);
+v_count number;
+begin
+v_sql := 'select count(*) from employee';
+execute immediate v_sql into v_count;
+dbms_output.put_line(v_count);
+end;
