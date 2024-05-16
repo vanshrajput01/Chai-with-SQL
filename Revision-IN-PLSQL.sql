6dd1cf9f075ee81b9172bacea7b4047f7e57dee4
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-05-16 10:44:57 +0530

    Revision-IN-PLSQL.sql 'conditional statement,CASE statement and Loop'

--- Revision-IN-PLSQL.sql
+++ Revision-IN-PLSQL.sql
@@ -33,3 +33,111 @@ dbms_output.put_line(v_emp_id || ' ' ||v_emp_name);
 end;
 
 
+--Conditional statementsin PLSQL
+
+declare
+v_num1 number := :num1;
+v_num2 number := :num2;
+v_sum number;
+begin
+if v_num1 != 0 and  v_num2 != 0 then
+v_sum := v_num1 + v_num2;
+dbms_output.put_line(v_sum);
+else
+dbms_output.put_line('give valid value of num1 and num2');
+end if;
+end;
+
+--Another example
+
+declare
+v_grade char := :grade;
+begin
+if v_grade = 'A' or v_grade = 'a' then
+dbms_output.put_line('Very good');
+elsif v_grade = 'B' or v_grade = 'b' then
+dbms_output.put_line('Good');
+elsif v_grade = 'C' or v_grade = 'c' then
+dbms_output.put_line('not bad');
+else
+dbms_output.put_line('Give me valid grade');
+end if;
+end;
+
+
+--Case statement 
+
+declare
+v_grade char := :grade;
+begin
+case v_grade
+when 'A' then
+dbms_output.put_line('Very good');
+when 'B' then
+dbms_output.put_line('Good');
+when 'C' then
+dbms_output.put_line('Not bad');
+else
+dbms_output.put_line('Give me valid grade!!');
+end case;
+end;
+
+--Another example of CASE statement
+declare
+v_age number := :your_age;
+begin
+case
+when v_age < 18 then
+dbms_output.put_line('Your age is not valid for Driving!!');
+when v_age >= 18 then
+dbms_output.put_line('Your age is valid for Drive!!');
+else
+dbms_output.put_line('Your age is not valid!!!');
+end case;
+end;
+
+--Loop in pl-sql;
+--Loop
+
+declare
+x int := 1;
+begin
+loop
+exit when x = 11;
+dbms_output.put_line(x);
+x := x + 1;
+end loop;
+end;
+
+--For loop in PL-SQL
+
+declare
+v_table int := :num;
+begin
+for i in 1..10 loop
+dbms_output.put_line(v_table * i);
+end loop;
+end;
+
+--While Loop in SQL
+
+declare
+v_end_num number := :end_num;
+i int := 2;
+begin
+while i <= v_end_num loop
+if mod(i,2) = 0 then
+dbms_output.put_line(i ||' is even');
+else
+dbms_output.put_line(i || ' is odd');
+end if;
+i := i+1;
+end loop;
+end;
+
+
+
+
+
+
+
