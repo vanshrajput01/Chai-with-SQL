b19c6a6eb0b75705eb1088266714bd8f40cc201f
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-06-20 16:04:00 +0530

    Generate in Json object in java

---
+++ Generate_Json.sql
@@ -0,0 +1,13 @@
+==================================================================== Generate json object in PL/SQL ====================================================================
+
+
+begin
+apex_json.initialize_clob_output;
+apex_json.open_object;
+apex_json.write('emp_name' , 'emp01');
+apex_json.write('emp_id' , '1');
+apex_json.close_object;
+dbms_output.put_line(apex_json.get_clob_output);
+end;
+
+=================================================================================================================================================================
