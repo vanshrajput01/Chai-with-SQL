f8a92923933ef71c992db4ffcaa6e1f15674eacf
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-06-20 16:07:07 +0530

    Generate object in side object in pl/sql

--- Generate_Json.sql
+++ Generate_Json.sql
@@ -10,4 +10,17 @@ apex_json.close_object;
 dbms_output.put_line(apex_json.get_clob_output);
 end;
 
-=================================================================================================================================================================
+====================================================================== Generate object in object ==============================================================
+
+begin
+apex_json.initialize_clob_output;
+apex_json.open_object('emp_details');
+apex_json.open_object;
+apex_json.write('emp_name' , 'emp01');
+apex_json.write('emp_id' , '1');
+apex_json.close_object;
+apex_json.close_object;
+dbms_output.put_line(apex_json.get_clob_output);
+end;
+
+
