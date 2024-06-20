7e59198f2c0e3a1878203bca1181ab4200871c72
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-06-20 16:14:10 +0530

    list in side multiple object

--- Generate_Json.sql
+++ Generate_Json.sql
@@ -23,4 +23,26 @@ apex_json.close_object;
 dbms_output.put_line(apex_json.get_clob_output);
 end;
 
+========================================================  Generate Array in side object ====================================================================
+
+
+
+begin
+apex_json.initialize_clob_output;
+apex_json.open_object('emp_details');
+apex_json.open_array;
+apex_json.open_object('emp_hdr_details');
+apex_json.write('emp_name' , 'emp01');
+apex_json.write('emp_id' , '1');
+apex_json.close_object;
+apex_json.open_object('emp_lines_details');
+apex_json.write('emp_name' , 'emp01');
+apex_json.write('emp_id' , '1');
+apex_json.close_object;
+apex_json.close_array;
+apex_json.close_object;
+dbms_output.put_line(apex_json.get_clob_output);
+end;
+
+
 
